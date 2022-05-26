11fad68f8921c577d156c2d9bba1cb63304c5ae0
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-26 15:33:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir -Şuara 78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1;
+            sayi1 = 3;
+            int sayi2;
+            sayi2 = 4;
+            int toplam;
+            toplam = sayi1 + sayi2;
+
+            Console.WriteLine("Sum Of Numbers");
+            Console.Write(toplam);
+            Console.Read();
+        }
+    }
+}
